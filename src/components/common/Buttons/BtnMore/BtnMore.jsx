3986c7a19b929fc42d1b1e23f3457913b3552c8a
Nikolay <NikolayBuch3@gmail.com>
2023-04-15 21:19:31 +0300

    bug fix2

--- src/components/common/Buttons/BtnMore/BtnMore.jsx
+++ src/components/common/Buttons/BtnMore/BtnMore.jsx
@@ -4,7 +4,7 @@ import s from './BtnMore.module.scss'
 const BtnMore = () => {
 	return (
 		<div>
-			<button className={s.root}>Подробнее<img src='./img/svg/Arrow.svg' alt="arrow"/></button>
+			<button className={s.root}>Подробнее<img src='./img/svg/arrow.svg' alt="arrow"/></button>
 		</div>
 	);
 };
